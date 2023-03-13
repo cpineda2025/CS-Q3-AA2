97135d39aa190d47b101276a06f48045aae8fd18
cpineda <93368167+cpineda2025@users.noreply.github.com>
2023-03-13 13:08:44 +0800

    Update WMGBSY.java

--- WMGBSY.java
+++ WMGBSY.java
@@ -1,5 +1,5 @@
-public class WMGBSY {
+public class Quiz_Interface {
     public static void main(String[]args){
-        System.out.println("Hello CLC!!!");
+        System.out.println("This is where ppl click buttons and it inputs stuff");
     }
 }
